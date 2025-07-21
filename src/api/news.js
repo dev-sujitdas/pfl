ccfa2e59584e2e11e51d0590ac09f59f84313663
Sujit Das <161117942+dev-sujitdas@users.noreply.github.com>
2025-07-21 21:02:33 +0530

    Update news.js

--- src/api/news.js
+++ src/api/news.js
@@ -1,6 +1,6 @@
 export const getFinancialNews = async () => {
   try {
-    const response = await fetch("https://mediastack-api-gamma.vercel.app/api/news");
+    const response = await fetch("https://gnews-api-k6.vercel.app/api/news");
     const result = await response.json();
     return result.data || [];
   } catch (error) {
