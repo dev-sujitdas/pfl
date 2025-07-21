9a559e52776a154a0c2709cb53633aca961a5469
Sujit Das <161117942+dev-sujitdas@users.noreply.github.com>
2025-07-21 21:32:01 +0530

    Update news.js

--- src/api/news.js
+++ src/api/news.js
@@ -2,7 +2,7 @@ export const getFinancialNews = async () => {
   try {
     const response = await fetch("https://gnews-api-k6.vercel.app/api/news");
     const result = await response.json();
-    return result.articles || [];
+    return result.data.articles || [];
   } catch (error) {
     console.log("Failed to fetch data", error);
     return [];
